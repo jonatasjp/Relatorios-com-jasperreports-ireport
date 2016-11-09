56f73d97de43e74eaad2d53b590ee69f02a7ec2a
jonatasLuis <jonatas_jp2@hotmail.com>
2016-11-08 23:32:39 -0200

    inicio da parte web do projeto

--- src/main/java/servlets/RelatorioServlet.java
+++ src/main/java/servlets/RelatorioServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/downloadRelatorio")
+@WebServlet("/downloadRelatorio.do")
 public class RelatorioServlet extends HttpServlet {
 
 	/**
